4acd0853861ab69cac68cae3a5298ec0170508d1
Dafydd Archard <dafyddarchard@dafydds-mbp.home>
2020-11-12 21:04:38 +0000

    no need to unpack request data for serializers

--- server/api_project/api/views.py
+++ server/api_project/api/views.py
@@ -31,8 +31,8 @@ def _handle_get_delete_update(request, pk, serializer, model):
         return Response(serialized.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-def _handle_post(request, data, serializer):
-    serialized = serializer(data=data)
+def _handle_post(request, serializer):
+    serialized = serializer(data=request.data)
     if serialized.is_valid():
         serialized.save()
         return Response(serialized.data, status=status.HTTP_201_CREATED)
@@ -48,8 +48,7 @@ def get_delete_update_user(request, pk):
 
 @api_view(['POST'])
 def create_user(request):
-    user_data = {'firebase_uid': request.data.get('firebase_uid')}
-    return _handle_post(request, user_data, UserSerializer)
+    return _handle_post(request, UserSerializer)
 
 
 # BOARD VIEWS
@@ -60,12 +59,7 @@ def get_delete_update_board(request, pk):
 
 @api_view(['POST'])
 def create_board(request):
-    board_data = {
-        'title': request.data.get('title'),
-        'user': request.data.get('user')
-    }
-
-    return _handle_post(request, board_data, BoardWriteSerializer)
+    return _handle_post(request, BoardWriteSerializer)
 
 
 # CARD LIST VIEWS
@@ -76,12 +70,7 @@ def get_delete_update_card_list(request, pk):
 
 @api_view(['POST'])
 def create_card_list(request):
-    card_list_data = {
-        'title': request.data.get('title'),
-        'board': request.data.get('board')
-    }
-
-    return _handle_post(request, card_list_data, CardListWriteSerializer)
+    return _handle_post(request, CardListWriteSerializer)
 
 
 # CARD VIEWS
@@ -92,11 +81,4 @@ def get_delete_update_card(request, pk):
 
 @api_view(['POST'])
 def create_card(request):
-    card_data = {
-        'title': request.data.get('title'),
-        'description': request.data.get('description'),
-        'position': request.data.get('position'),
-        'card_list': request.data.get('card_list')
-    }
-
-    return _handle_post(request, card_data, CardWriteSerializer)
+    return _handle_post(request, CardWriteSerializer)
